c90f0f57a56f513fd9beefc1808c565f2c0a657c
Javier Gispert <j.gispertrubio@gmail.com>
2022-06-30 11:03:39 +0200

    lab-started

--- main.py
+++ main.py
@@ -1,8 +1,69 @@
 #imports
+from modules import module as md
 
+API_TOKEN = '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' #API TOKEN (REMEMBER: do not push these to your repo)
+USERNAME = 'javiergispert' #USERNAME
+BASE_URL = 'https://api.github.com/'
+KEY = 'repos/'
+OWNER = 'ih-datapt-mad/'
+REPO = 'dataptmad0522_labs' #LAB_REPOSITORY
+SEARCH = 'search/issues?q=repo:'+OWNER+REPO+'+type:pr+state:{}'
+PULLS = 'pulls?page={}&per_page=100&state={}'
+COMMITS = 'pulls/{}/commits'
+STATE = 'open'
+
+field_list1 = ['number',
+               'title',
+               'state',
+               'created_at',
+               'updated_at',
+               'closed_at',
+               'html_url',
+               'base.repo.full_name',
+               'base.ref',
+               'head.repo.full_name',
+               'head.ref',
+               'head.repo.pushed_at']
+
+field_list2 = ['student_name',
+               'number',
+               'lab_name',
+               'state',
+               'lab_status',
+               'created_at',
+               'updated_at',
+               'closed_at',
+               'html_url',
+               'base.repo.full_name',
+               'base.ref',
+               'head.repo.full_name',
+               'head.ref',
+               'head.repo.pushed_at']
+
+field_sort1 = ['lab_status',
+               'lab_name',
+               'student_name']
+
+field_name1 = ['Student Name',
+               'PR Number',
+               'Lab Name',
+               'PR Status',
+               'Lab Status',
+               'PR Created at',
+               'PR Updated at',
+               'PR Closed at',
+               'PR URL',
+               'base repository',
+               'base',
+               'head repository',
+               'compare',
+               'Pushed at']
 
 def main():
   
 
 if __name__ == '__main__':
-    main()
+    DF_PULLS = get_pulls(BASE_URL, KEY, OWNER, REPO, PULLS, SEARCH, STATE, USERNAME, API_TOKEN, field_list1)
+    DF_STATUS = df_status(DF_PULLS, BASE_URL, KEY, OWNER, REPO, COMMITS, USERNAME, API_TOKEN, field_list2)
+    DF_CSV = create_csv(DF_STATUS, field_sort1, field_name1)
+    DF_CSV
